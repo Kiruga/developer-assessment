fe376815e4c98a224156ee89ff54665cf3fdeee4
Маскалёв Кирилл <kmaskalev72@gmail.com>
2024-06-17 11:34:25 +0300

    Task 4 - added fields to Policy and Note models to create a one-to-many relationship - configured Note and one-to-many relationship on ExporterDbContext

--- DataExporter/ExporterDbContext.cs
+++ DataExporter/ExporterDbContext.cs
@@ -8,6 +8,8 @@ namespace DataExporter
     {
         public DbSet<Policy> Policies { get; set; }
 
+        public DbSet<Note> Notes { get; set; }
+
         public ExporterDbContext(DbContextOptions<ExporterDbContext> options) : base(options)
         { 
         }
@@ -19,11 +21,19 @@ namespace DataExporter
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Policy>().HasMany(e => e.Notes).WithOne(e => e.Policy).HasForeignKey(e => e.PolicyId).IsRequired(false);
+
             modelBuilder.Entity<Policy>().HasData(new Policy() { Id = 1, PolicyNumber = "HSCX1001", Premium = 200, StartDate = new DateTime(2024, 4, 1) },
                 new Policy() { Id = 2, PolicyNumber = "HSCX1002", Premium = 153, StartDate = new DateTime(2024, 4, 5) },
                 new Policy() { Id = 3, PolicyNumber = "HSCX1003", Premium = 220, StartDate = new DateTime(2024, 3, 10) },
                 new Policy() { Id = 4, PolicyNumber = "HSCX1004", Premium = 200, StartDate = new DateTime(2024, 5, 1) },
                 new Policy() { Id = 5, PolicyNumber = "HSCX1005", Premium = 100, StartDate = new DateTime(2024, 4, 1) });
+
+            modelBuilder.Entity<Note>().HasData(
+                new Note() { Id = 1, PolicyId = 1, Text = "Note 1-1" },
+                new Note() { Id = 2, PolicyId = 1, Text = "Note 1-2" },
+                new Note() { Id = 3, PolicyId = 2, Text = "Note 2-1" }
+                );
 
             base.OnModelCreating(modelBuilder);
         }
